6302504c8f3f675b787b80694247b1c72aa60cce
abdallh <heba.mamdooh975@gmail.com>
2024-11-23 13:14:33 +0200

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,20 +1,65 @@
 #!/usr/bin/node
-const util = require('util');
-const request = util.promisify(require('request'));
-const filmID = process.argv[2];
 
-async function starwarsCharacters (filmId) {
-  const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
-  let response = await (await request(endpoint)).body;
-  response = JSON.parse(response);
-  const characters = response.characters;
+const request = require('request');
 
-  for (let i = 0; i < characters.length; i++) {
-    const urlCharacter = characters[i];
-    let character = await (await request(urlCharacter)).body;
-    character = JSON.parse(character);
-    console.log(character.name);
-  }
+// Get the Movie ID from command-line arguments
+const movieId = process.argv[2];
+if (!movieId) {
+  console.error('Usage: ./0-starwars_characters.js <movie_id>');
+  process.exit(1);
 }
 
-starwarsCharacters(filmID);
+// API URL for the given movie ID
+const filmUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+// Fetch movie details
+request(filmUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error(`Failed to fetch data: ${response.statusCode}`);
+    return;
+  }
+
+  const filmData = JSON.parse(body);
+  const characterUrls = filmData.characters;
+
+  // Fetch each character's name in order
+  fetchCharacters(characterUrls);
+});
+
+// Function to fetch character names
+function fetchCharacters(urls) {
+  const names = [];
+
+  const fetchCharacter = (index) => {
+    if (index >= urls.length) {
+      // All names fetched, print them
+      names.forEach(name => console.log(name));
+      return;
+    }
+
+    request(urls[index], (error, response, body) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      if (response.statusCode !== 200) {
+        console.error(`Failed to fetch data: ${response.statusCode}`);
+        return;
+      }
+
+      const character = JSON.parse(body);
+      names.push(character.name);
+
+      // Fetch the next character
+      fetchCharacter(index + 1);
+    });
+  };
+
+  fetchCharacter(0); // Start with the first character
+}
