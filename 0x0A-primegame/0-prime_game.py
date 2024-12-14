0d8352beb5ce7df18e35045a805ed2cca5582a75
heba <52792349+HEBAMamdooh@users.noreply.github.com>
2024-12-14 21:44:40 +0200

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,52 +2,45 @@
 """Prime Game Module"""
 
 
-def sieve_of_eratosthenes(limit):
-    """Generate a list of primes up to 'limit'
-    using the Sieve of Eratosthenes."""
-    sieve = [True] * (limit + 1)
-    sieve[0], sieve[1] = False, False  # 0 and 1 are not prime numbers
-    for num in range(2, int(limit ** 0.5) + 1):
-        if sieve[num]:
-            for multiple in range(num * num, limit + 1, num):
-                sieve[multiple] = False
-    return [num for num, is_prime in enumerate(sieve) if is_prime]
-
-
-def prime_game(n):
-    """Simulate the prime game for a given 'n'
-    and return the winner ('Maria' or 'Ben')."""
-    primes = sieve_of_eratosthenes(n)
-    available = [True] * (n + 1)
-    turn = 0  # 0 for Maria's turn, 1 for Ben's turn
-
-    while True:
-        for prime in primes:
-            if prime <= n and available[prime]:
-                # The current player picks this prime
-                #  and removes it and its multiples
-                for i in range(prime, n + 1, prime):
-                    available[i] = False
-                # Switch turns
-                turn = 1 - turn
-                break
-        else:
-            # No more primes to pick, game over, current player loses
-            return 'Maria' if turn == 1 else 'Ben'
-
-
 def isWinner(x, nums):
-    """Determine the winner of x rounds given the nums array."""
+    """Determine the winner of the prime game."""
+    # Validate input
+    if not nums or x <= 0 or any(n < 0 for n in nums):
+        return None
+
+    # Determine the maximum value of n in nums
+    max_n = max(nums)
+
+    # Handle edge cases where no valid game can be played
+    if max_n < 2:
+        return None  # No primes, no game
+
+    # Sieve of Eratosthenes to precompute primes up to max_n
+    primes = [True] * (max_n + 1)
+    primes[0] = primes[1] = False  # 0 and 1 are not primes
+    for i in range(2, int(max_n**0.5) + 1):
+        if primes[i]:
+            for j in range(i * i, max_n + 1, i):
+                primes[j] = False
+
+    # Precompute the number of primes up to each number
+    prime_counts = [0] * (max_n + 1)
+    for i in range(1, max_n + 1):
+        prime_counts[i] = prime_counts[i - 1] + (1 if primes[i] else 0)
+
+    # Game rounds and determining the winner
     maria_wins = 0
     ben_wins = 0
 
     for n in nums:
-        winner = prime_game(n)
-        if winner == 'Maria':
-            maria_wins += 1
-        else:
+        if n < 2:  # If n is less than 2, Ben automatically wins this round
             ben_wins += 1
+        elif prime_counts[n] % 2 == 0:
+            ben_wins += 1
+        else:
+            maria_wins += 1
 
+    # Determine the winner
     if maria_wins > ben_wins:
         return 'Maria'
     elif ben_wins > maria_wins:
